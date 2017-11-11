d334b665e5204b521b8fa867a134fafcb856dbf9
georgi.nik95@gmail.com <georgi.nik95@gmail.com>
2017-11-11 23:59:03 +0200

    Add controllers for api ai and microsoft bot

---
+++ Nobby.ChatBot/Controllers/ApiAiController.cs
@@ -0,0 +1,25 @@
+﻿using ApiAiSDK.Model;
+using System.Web.Http;
+using Nobby.ChatBot.Helpers;
+
+namespace Nobby.ChatBot.Controllers
+{
+    public class ApiAiController : ApiController
+    {
+        public dynamic Post(AIResponse aiResponse)
+        {
+            var commonModel = CommonModelMapper.ApiAiToCommonModel(aiResponse);
+            if (commonModel == null)
+                return null;
+
+            commonModel = IntentRouter.Process(commonModel);
+
+            return CommonModelMapper.CommonModelToApiAi(commonModel);
+        }
+
+        public string Get()
+        {
+            return "Hello API.AI!";
+        }
+    }
+}
